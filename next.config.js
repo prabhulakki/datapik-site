78d5cd2a5697bfbc3b8829c51012b8115afb8478
prabhulakki <108394442+prabhulakki@users.noreply.github.com>
2025-10-01 21:25:08 +0100

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'robohash.org',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos'
+      }
+    ]
+  }
+}
+
+module.exports = nextConfig
